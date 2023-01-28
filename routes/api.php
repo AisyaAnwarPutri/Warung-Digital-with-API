4cbb961bc249b7c7d4407aa0f25fe44ba864b8c0
AisyaAnwarPutri <aisyanwarputri@gmail.com>
2023-01-28 15:45:45 +0700

    API Pesanan

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\SliderController;
@@ -32,7 +33,15 @@ Route::group([
         'products' => ProductController::class,
         'members' => MemberController::class,
         'testimonis' => TestimoniController::class,
-        'reviews' => ReviewController::class
+        'reviews' => ReviewController::class,
+        'orders' => OrderController::class,
 
     ]);
+
+    Route::get('order/dikonfirmasi', [OrderController::class, 'dikonfirmasi']);
+    Route::get('order/dikemas', [OrderController::class, 'dikemas']);
+    Route::get('order/dikirim', [OrderController::class, 'dikirim']);
+    Route::get('order/diterima', [OrderController::class, 'diterima']);
+    Route::get('order/selesai', [OrderController::class, 'selesai']);
+    Route::post('order/ubah_status/{order}', [OrderController::class, 'ubah_status']);
 });
